1781b7c244fae4c573e0a8de4dba1046f8730a61
tarun <tarunreddy.b@enterpi.com>
2025-09-05 14:41:27 +0530

    auth ui is done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 const jost = Jost({
   variable: "--font-jost",
@@ -21,8 +19,7 @@ export default function RootShell({
 }>) {
   return (
     <html lang="en">
-      <body className={`${jost.className} antialiased`}>
-        <Navbar/>{children}<Footer/></body>
+      <body className={`${jost.className} antialiased`}>{children}</body>
     </html>
   );
 }
